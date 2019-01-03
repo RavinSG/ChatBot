31d13009483e652f77f65d9acff2ae2181c64ed7
Ravin Gunawardena <RavinSG@users.noreply.github.com>
2019-01-04 01:49:07 +0530

    Map user details to answers

--- user_db.py
+++ user_db.py
@@ -1,10 +1,13 @@
 import sqlite3
+import json
 
 sqlite_file = 'user_data.sqlite'
 username = 'Ravin'
 
 conn = sqlite3.connect(sqlite_file)
 c = conn.cursor()
+
+stop_words = json.load(open('stopwords-en.json', 'r', encoding='utf-8'))
 
 
 def create_user(new_user):
@@ -25,13 +28,47 @@ def create_user(new_user):
     return names
 
 
-def add_entry(question, answer):
+def remove_stop_words(phrase):
+    keys = []
+    phrase = phrase.lower().strip('?').split(' ')
+    for word in phrase:
+        if word not in stop_words:
+            keys.append(word)
+    return ' '.join(keys)
+
+
+def add_entry(answer):
+    question = remove_stop_words(answer)
     c.execute('INSERT INTO Ravin VALUES (?, ?)',
               (question, answer))
     conn.commit()
     return None
 
 
-create_user(username)
+def change_pronouns(phrase):
+    source = ['i', 'am', 'i\'m', 'my']
+    target = ['you', 'are', 'you\'re', 'your']
+    phrase = phrase.lower().split(' ')
+    for i in range(len(phrase)):
+        if phrase[i] in source:
+            phrase[i] = target[source.index(phrase[i])]
+    return ' '.join(phrase)
 
-add_entry('df', '123')
+
+def answer_question(question):
+    keys = remove_stop_words(question).split(' ')
+    max_score = 0
+    answer = c.execute('SELECT * from {tn}'.format(tn =username)).fetchall()
+    reply = ''
+    for ans in answer:
+        score = 0
+        q_keys = ans[0].split(' ')
+        for key in keys:
+            if key in q_keys:
+                score += 1
+        if score >= max_score:
+            max_score = score
+            reply = ans[1]
+    print(change_pronouns(reply))
+
+answer_question('Where do i study?')
